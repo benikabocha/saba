d7f9a8c4313506616eef4c669a678b058fb70cb1
benikaboha <benikaboha@gmail.com>
2016-11-23 02:07:08 +0900

    gtest で glfw のウィンドウを初期化する際、 Core Profile を指定するようにした。 mac では Core Profile を指定しないとウィンドウが作成されないため。

--- gtests/main.cpp
+++ gtests/main.cpp
@@ -19,6 +19,11 @@ int main(int argc, char** argv)
 		std::cout << "error: glfwInit fail.\n";
 		return 1;
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (window == nullptr)
 	{
