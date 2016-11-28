1c661c3a9e0646ed9f2cb0161a76b596062120fa
benikaboha <benikaboha@gmail.com>
2016-11-28 23:46:02 +0900

    Time を追加

---
+++ src/Saba/Base/Time.cpp
@@ -0,0 +1,68 @@
+﻿#include "Time.h"
+
+#include "Singleton.h"
+
+#if _WIN32
+#include <Windows.h>
+#else // _WIN32
+#include <time.h>
+#endif // _WIN32
+
+namespace saba
+{
+	namespace
+	{
+#if _WIN32
+		class Time
+		{
+		public:
+			Time()
+			{
+				QueryPerformanceFrequency(&m_freq);
+			}
+
+			double GetTime() const
+			{
+				LARGE_INTEGER count;
+				QueryPerformanceCounter(&count);
+
+				return (double)count.QuadPart / (double)m_freq.QuadPart;
+			}
+
+		private:
+			LARGE_INTEGER	m_freq;
+		};
+#else // _WIN32
+		class Time
+		{
+		public:
+			Time()
+			{
+			}
+
+			double GetTime() const
+			{
+				timespec ts;
+				clock_gettime(CLOCK_MONOTONIC, &ts);
+				uint64_t time = (uint64_t)tv.tv_sec * 1000000000 + (uint64_t)tv.tv_usec;
+				return (double)time / 1000000000.0;
+			}
+		};
+#endif // _WIN32
+	}
+
+	double GetTime()
+	{
+		return Singleton<Time>::Get()->GetTime();
+	}
+
+	double GetTimeMSec()
+	{
+		return GetTime() * 1000.0;
+	}
+
+	double GetTimeUSec()
+	{
+		return GetTimeMSec() * 1000.0;
+	}
+}
---
+++ src/Saba/Base/Time.h
@@ -0,0 +1,11 @@
+﻿#ifndef SABA_BASE_TIME_H_
+#define SABA_BASE_TIME_H_
+
+namespace saba
+{
+	double GetTime();
+	double GetTimeMSec();
+	double GetTimeUSec();
+}
+
+#endif // !SABA_BASE_TIME_H_
