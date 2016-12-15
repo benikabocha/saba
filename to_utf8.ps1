72dbd300e9e5d1172a2477b3bc5c84397ee0d0cf
benikaboha <benikaboha@gmail.com>
2016-12-15 23:24:54 +0900

    バックアップファイルを削除するようにした

--- to_utf8.ps1
+++ to_utf8.ps1
@@ -1,11 +1,43 @@
-Get-ChildItem .\src -Recurse -Include *.cpp | Rename-Item -NewName {$_.FullName + ".bkup"}
-Get-ChildItem .\src -Recurse -Include *.cpp.bkup | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding UTF8 ($_.FullName -replace '.cpp.bkup','.cpp')}
+Get-ChildItem .\src -Recurse -Include *.cpp |
+	Rename-Item -NewName {
+		$_.FullName + ".bkup"
+	}
+Get-ChildItem .\src -Recurse -Include *.cpp.bkup |
+	ForEach-Object {
+		Get-Content $_.FullName |
+			Out-File -Encoding UTF8 ($_.FullName -replace '.cpp.bkup','.cpp')
+			Remove-Item $_.FullName
+	}
 
-Get-ChildItem .\src -Recurse -Include *.h | Rename-Item -NewName {$_.FullName + ".bkup"}
-Get-ChildItem .\src -Recurse -Include *.h.bkup | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding UTF8 ($_.FullName -replace '.h.bkup','.h')}
+Get-ChildItem .\src -Recurse -Include *.h |
+	Rename-Item -NewName {
+		$_.FullName + ".bkup"
+	}
+Get-ChildItem .\src -Recurse -Include *.h.bkup |
+	ForEach-Object {
+		Get-Content $_.FullName |
+			Out-File -Encoding UTF8 ($_.FullName -replace '.h.bkup','.h')
+			Remove-Item $_.FullName
+	}
 
-Get-ChildItem .\gtests -Recurse -Include *.cpp | Rename-Item -NewName {$_.FullName + ".bkup"}
-Get-ChildItem .\gtests -Recurse -Include *.cpp.bkup | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding UTF8 ($_.FullName -replace '.cpp.bkup','.cpp')}
+Get-ChildItem .\gtests -Recurse -Include *.cpp |
+	Rename-Item -NewName {
+		$_.FullName + ".bkup"
+	}
+Get-ChildItem .\gtests -Recurse -Include *.cpp.bkup |
+	ForEach-Object {
+		Get-Content $_.FullName |
+			Out-File -Encoding UTF8 ($_.FullName -replace '.cpp.bkup','.cpp')
+			Remove-Item $_.FullName
+	}
 
-Get-ChildItem .\gtests -Recurse -Include *.h | Rename-Item -NewName {$_.FullName + ".bkup"}
-Get-ChildItem .\gtests -Recurse -Include *.h.bkup | ForEach-Object {Get-Content $_.FullName | Out-File -Encoding UTF8 ($_.FullName -replace '.h.bkup','.h')}
+Get-ChildItem .\gtests -Recurse -Include *.h |
+	Rename-Item -NewName {
+		$_.FullName + ".bkup"
+	}
+Get-ChildItem .\gtests -Recurse -Include *.h.bkup |
+	ForEach-Object {
+		Get-Content $_.FullName |
+			Out-File -Encoding UTF8 ($_.FullName -replace '.h.bkup','.h')
+			Remove-Item $_.FullName
+	}
