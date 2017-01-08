81863b2c4097516fb17d0282f8b237ccf3cccc21
benikabocha <benikabocha@gmail.com>
2017-01-08 15:57:43 +0900

    初期設定で MSAA を有効にできるようにした

--- main.cpp
+++ main.cpp
@@ -7,11 +7,53 @@
 
 #include <Saba/Viewer/Viewer.h>
 
+#include <picojson.h>
+#include <fstream>
+
 int main()
 {
 	SABA_INFO("Start");
 
+	bool msaaEnable = false;
+	int msaaCount = 4;
+	{
+		std::ifstream initJsonFile;
+		initJsonFile.open("init.json");
+		if (initJsonFile.is_open())
+		{
+			picojson::value val;
+			initJsonFile >> val;
+			initJsonFile.close();
+
+			auto& init = val.get<picojson::object>();
+			if (val.contains("MSAAEnable"))
+			{
+				msaaEnable = init["MSAAEnable"].get<bool>();
+			}
+			if (val.contains("MSAACount"))
+			{
+				double count = init["MSAACount"].get<double>();
+				msaaCount = (int)(count + 0.5);
+			}
+		}
+	}
+
 	saba::Viewer viewer;
+
+	if (msaaEnable)
+	{
+		SABA_INFO("Enable MSAA");
+		viewer.EnableMSAA(true);
+
+		if (msaaCount != 2 && msaaCount != 4 && msaaCount != 8)
+		{
+			SABA_WARN("MSAA Count Force Change : 4");
+			msaaCount = 4;
+		}
+
+		SABA_INFO("MSAA Count : {}", msaaCount);
+		viewer.SetMSAACount(msaaCount);
+	}
 
 	if (!viewer.Initialize())
 	{
