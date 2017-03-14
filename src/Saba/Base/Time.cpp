f46ce3a12cf6ed88ca723e39a04da94347bfa269
benikabocha <benikabocha@gmail.com>
2017-03-15 01:09:45 +0900

    high_resolution_clock を使用し GetTime を実装

--- src/Saba/Base/Time.cpp
+++ src/Saba/Base/Time.cpp
@@ -5,69 +5,25 @@
 
 #include "Time.h"
 
-#include "Singleton.h"
-
-#if _WIN32
-#include <Windows.h>
-#else // _WIN32
-#include <time.h>
-#endif // _WIN32
+#include <chrono>
 
 namespace saba
 {
-	namespace
-	{
-#if _WIN32
-		class Time
-		{
-		public:
-			Time()
-			{
-				QueryPerformanceFrequency(&m_freq);
-			}
-
-			double GetTime() const
-			{
-				LARGE_INTEGER count;
-				QueryPerformanceCounter(&count);
-
-				return (double)count.QuadPart / (double)m_freq.QuadPart;
-			}
-
-		private:
-			LARGE_INTEGER	m_freq;
-		};
-#else // _WIN32
-		class Time
-		{
-		public:
-			Time()
-			{
-			}
-
-			double GetTime() const
-			{
-				timespec ts;
-				clock_gettime(CLOCK_MONOTONIC, &ts);
-				uint64_t time = (uint64_t)ts.tv_sec * 1000000000 + (uint64_t)ts.tv_nsec;
-				return (double)time / 1000000000.0;
-			}
-		};
-#endif // _WIN32
-	}
-
 	double GetTime()
 	{
-		return Singleton<Time>::Get()->GetTime();
+		auto now = std::chrono::high_resolution_clock::now();
+		return std::chrono::duration<double>(now.time_since_epoch()).count();
 	}
 
 	double GetTimeMSec()
 	{
-		return GetTime() * 1000.0;
+		auto now = std::chrono::high_resolution_clock::now();
+		return std::chrono::duration<double, std::milli>(now.time_since_epoch()).count();
 	}
 
 	double GetTimeUSec()
 	{
-		return GetTimeMSec() * 1000.0;
+		auto now = std::chrono::high_resolution_clock::now();
+		return std::chrono::duration<double, std::micro>(now.time_since_epoch()).count();
 	}
 }
