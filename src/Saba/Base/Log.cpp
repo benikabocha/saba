899381fb4d7e60747b2de2c36b16f1a1a1bdff82
benikabocha <benikabocha@gmail.com>
2019-02-14 01:22:38 +0900

    Set the output code page UTF-8.(Windows)

--- src/Saba/Base/Log.cpp
+++ src/Saba/Base/Log.cpp
@@ -19,43 +19,14 @@ namespace saba
 	DefaultSink::DefaultSink()
 	{
 		m_defaultLogger = spdlog::stdout_color_mt("default");
+#if _WIN32
+		SetConsoleOutputCP(CP_UTF8);
+#endif // _WIN32
 	}
 
 	void DefaultSink::log(const spdlog::details::log_msg & msg)
 	{
-#if _WIN32
-		auto utf8Message = msg.raw.str();
-		std::wstring wMessage;
-		if (!TryToWString(utf8Message, wMessage))
-		{
-			m_defaultLogger->log(msg.level, "Failed to convert message.");
-			return;
-		}
-		int chCount = WideCharToMultiByte(
-			CP_OEMCP,
-			0,
-			wMessage.c_str(),
-			(int)wMessage.size(),
-			nullptr,
-			0,
-			0,
-			FALSE
-		);
-		std::string oemMessage(chCount, '\0');
-		WideCharToMultiByte(
-			CP_OEMCP,
-			0,
-			wMessage.c_str(),
-			(int)wMessage.size(),
-			&oemMessage[0],
-			chCount,
-			0,
-			FALSE
-		);
-		m_defaultLogger->log(msg.level, oemMessage);
-#else // _WIN32
 		m_defaultLogger->log(msg.level, msg.raw.c_str());
-#endif // _WIN32
 	}
 
 	void DefaultSink::flush()
